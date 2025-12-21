63ac0c0fc82aaeee056b5dea5a977ccfa6692668
Girija <giricivil551@gmail.com>
2025-12-21 19:02:32 +0530

    aws keys included in s3 module

--- cloud/s3_utils.py
+++ cloud/s3_utils.py
@@ -11,6 +11,8 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 s3 = boto3.client(
     "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     region_name=os.getenv("AWS_REGION"),
     config=Config(signature_version="s3v4")
 )
